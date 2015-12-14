331c1b5a64fc89ccf726a7a924904feffa35b65e
Christian Bolstad <christian@bolstad.se>
2015-12-14 22:04:17 +0100

    session

---
+++ src/SessionStoragePhpSession.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christian
+ * Date: 2015-12-14
+ * Time: 22:03
+ */
+
+namespace Analytrix;
+
+
+class SessionStoragePhpSession implements SessionStorageInterface
+{
+    public function setBucket($bucketName)
+    {
+        // TODO: Implement setBucket() method.
+    }
+
+    public function __get($key)
+    {
+        // TODO: Implement __get() method.
+    }
+
+    public function __set($key, $value)
+    {
+        // TODO: Implement __set() method.
+    }
+}

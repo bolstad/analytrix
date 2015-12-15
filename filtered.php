ac064cbf19653d3a58d176d99d040ffcbbd09cf8
Christian Bolstad <christian@carnaby.se>
2015-12-15 17:56:12 +0100

    filtererd

---
+++ filtered.php
@@ -0,0 +1,54 @@
+<?php
+
+date_default_timezone_set( 'Europe/Stockholm' );
+session_start();
+
+include 'vendor/autoload.php';
+
+$ding = new Analytrix\Basic(  new \Dotenv\Dotenv(__DIR__), new \timgws\GoogleAnalytics\API, new Analytrix\SessionStorageFile);
+
+$ding->storage->setBucket('christian@carnaby.se');
+$ACCOUNT_ID =  getenv( 'ACCOUNT_ID' );
+echo "ACCOUNT_ID: $ACCOUNT_ID\n";
+
+$ding->LoginText = 'Please login here';
+$ding->DieOnNoSession = true;
+
+$ding->run();
+
+$auth = $ding->storage->get('auth');
+
+if ( $auth = $ding->storage->get('auth') ) {
+
+    echo "Yes, session is set\n";
+
+    print_r( $auth );
+
+    $accessToken = $auth['access_token'];
+    $tokenExpires = $auth['expires_in'];
+
+    $ding->ga->setAccessToken( $accessToken );
+    $ding->ga->setAccountId( $ACCOUNT_ID );
+
+    // Set the default params. For example the start/end dates and max-results
+    $defaults = array(
+        'start-date' => date( 'Y-m-d', strtotime( '-1 month' ) ),
+        'end-date' => date( 'Y-m-d' ),
+    );
+    $ding->ga->setDefaultQueryParams( $defaults );
+
+    // Example1: Get visits by date
+    $params = array(
+        'metrics' => 'ga:visits',
+        'dimensions' => 'ga:month',
+    );
+    $visits = $ding->ga->query( $params );
+
+    echo '<pre>';
+    print_r( $visits );
+    echo '</pre>';
+    die;
+}
+
+
+
